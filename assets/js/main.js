07115129caa0a41726a312957f5236681b3a8580
AKmahim <mdmahimhossen134@gmail.com>
2022-03-11 19:05:49 +0600

    New Template

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// ====== slider js ==========
+
+$('.slider-item').slick({
+    // dots: true,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    // centerMode: true,
+    // variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+  });
