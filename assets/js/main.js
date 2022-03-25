d1757683d7c24d697f64aadf884f4bfdee698abf
AKmahim <mdmahimhossen134@gmail.com>
2022-03-25 19:51:00 +0600

    03/25/22

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,25 @@ $('.slider-item').slick({
     autoplaySpeed: 3000,
 
   });
+
+
+
+
+  // ============ preloader js code =======
+
+// var preloader = document.getElementById("preloader");
+
+
+// window.addEventListener("load",()=>{
+// 	preloader.style.display = "none";
+// })
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
